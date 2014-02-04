a8b354d53b6788e0e95947321e344c16500d4c86
alhenk <alexandr.g.koryagin@gmail.com>
2014-02-04 11:44:48 +0600

    insignificant

--- src/com/epam/koryagin/matrix/Runner.java
+++ src/com/epam/koryagin/matrix/Runner.java
@@ -1,14 +1,15 @@
 package com.epam.koryagin.matrix;
 
 public class Runner {
+		
 	public static void main(String[] args) {
 		Node<Double> node = new Node<Double>(10.2);
 		System.out.println("ID = " + node.getId());
 		System.out.println("VALUE = " + node.getValue());
-		int dimension = 3;
+		int dimension = 5;
 		Matrix<Node<Double>> matrix = 
 				new Matrix<Node<Double>>(dimension, node);
 		
-		System.out.println(matrix);
+		System.out.println(matrix);	
 	}
 }
